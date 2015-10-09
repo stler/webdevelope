f21433b34855f350ac3a812136817b6c3710269c
Nikolay Akopov <asoman97@gmail.com>
2015-10-09 23:59:21 +0400

    Ужасное костыляние и наглый тест возможностей в индексе

--- lesson2/classes/MySqlDBClass.php
+++ lesson2/classes/MySqlDBClass.php
@@ -1,35 +1,105 @@
-<?php
-class MySqlDB {
-     private $manager;
-     public function __construct($host, $dbname,  $username, $password = null){
-        $dsn = "mysql:host=". $host . ";dbname=" . $dbname . ";" ;
-        $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
-         try {
-        $this->manager = new PDO($dsn, $username, $password, $options);
+ï»¿<?php 
+class MySql {
+	private $manager;
+	public function __construct($host, $dbname, $username, $password = null)
+	{
+		$dsn = "mysql:host=". $host. ";dbname=". $dbname. ";";
+		$options =  array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+ PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
+		try {
+		$this->manager = new PDO($dsn, $username, $password, $options);
+		} catch (PDOException $e) {
+			die('ÐŸÐ¾Ð´ÐºÐ»ÑŽÑ‡ÐµÐ½Ð¸Ðµ Ð½Ðµ ÑƒÐ´Ð°Ð»Ð¾ÑÑŒ: ' . $e->getMessage());
+		}
+	}
+	public function selectAll()
+	{
 
-         } catch (PDOException $e) {
-             die('Ïîäêëþ÷åíèå íå óäàëîñü: ' . $e->getMessage());
-         }
+		$stmt = "SELECT * FROM user";
+		return $this->manager->query($stmt);
+	}
+	public function insert ($table, $data)
+	{
+		$columns = array();
+		$values = array();
+	
+		foreach ($data as $key => $value)
+		{
+			$key = mysql_real_escape_string($key . '');
+			$columns[] = $key;
+			
+			if ($value === null)
+			{
+				$values[] = 'NULL';
+			}
+			else
+			{
+				$value = mysql_real_escape_string($value . '');							
+				$values[] = "'$value'";
+			}
+		}
+		
+		$columns_s = implode(',', $columns);
+		$values_s = implode(',', $values);
+		
+		$query = "INSERT INTO $table ($columns_s) VALUES ($values_s)";
+		$result = $this->manager->query($query);
+		return $this->manager->lastInsertId();
+	}
+	
+	public function select($query)
+	{
 
+		$result = $this->manager->query($query);
+		
+		if (!$result)
+			die(mysqli_error());
+		
+		return $result;
+		
+	}
+	
+	public function delete($table, $where)
+	{
+
+		$query = "DELETE FROM $table WHERE $where";
+		$result = $this->manager->query($query);
+		
+		if (!$result)
+			die(mysqli_error());
+		
+		return $result->rowCount();
+		
+	}
+	
+	public function update($table, $object, $where)
+	{
+
+		$sets = array();
+	
+		foreach ($object as $key => $value)
+		{
+			$key = mysql_real_escape_string($key . '');
+			
+			if ($value === null)
+			{
+				$sets[] = "$key=NULL";			
+			}
+			else
+			{
+				$value = mysql_real_escape_string($value . '');					
+				$sets[] = "$key='$value'";			
+			}			
+		}
+		
+		$sets_s = implode(',', $sets);			
+		$query = "UPDATE $table SET $sets_s WHERE $where";
+		$result = $this->manager->query($query);
+		
+		if (!$result)
+			die(mysql_error());
+		return $result->rowCount();	
+		
+	}
 }
-    public function selectAll(){
-        $stmt = "SELECT name FROM user";
-        return $this->manager->query($stmt);
-    }
-    public function insert( $data){
-        $stmt = "INSERT INTO user VALUES " . $data . ";" ;
-        $this->manager->query($stmt);
-    }
-
-    public  function delete($deletevalue){
-        $stmt = "DELETE FROM user WHERE login = '" . $deletevalue . "'";
-        $this->manager->query($stmt);
-    }
-    public function update($changefield,$newvalue,$login){
-        $stmt = "UPDATE user SET " . $changefield . "= '" . $newvalue ."' WHERE (login='". $login ."');" ;
-        $this->manager->query($stmt);
-    }
-    }
-
 ?>
--- lesson2/index.php
+++ lesson2/index.php
@@ -1,65 +1,40 @@
 <?php 
-$dbname = "lesson2"; 
-$host = "localhost"; 
-$userName = "root"; 
-require_once("classes\\MySqlDBClass.php"); 
-$pdo = new MySqlDB($host, $dbname, $userName ); 
-//$pdostmt = $pdo->selectAll();  ����� ����� �� ������ ������ �����
-//while($row = $pdostmt->fetch()){ 
-//echo $row['name'] ; 
-//} 
+//Накостылял, прям страшно. Чтот подумываю перестать ходить на Пыху, а тратить это время на изучение рельс.
+$dbname = "lesson_2";
+$table = "user";
+$host = "localhost";
+$username = "root";
+require_once("classes\\MySqlDBClass.php");
+$pdo = new MySql($host, $dbname, $username);
+$pdostmt = $pdo->selectAll();
+while ($row = $pdostmt->fetch())
+{
+	echo $row['name']. "\n";
+}	
+$pdostmt = $pdo->insert('user',array('name'=>'fourth', 'password'=>"pass"));
+echo '<div>'.$pdostmt.'</div>';
+$pdostmt = $pdo->selectAll();
 
 
-if ( isset($_POST["Create"] ) && $_POST["Name"]!= null && $_POST["login"]!= null && $_POST["password"]!= null && $_POST["email"]!= null ){ 
-$data = "(0,'" . $_POST["Name"] . "','" . $_POST["login"] ."','". $_POST["password"] . "','" . $_POST["email"] ."');"; 
-$pdo->insert($data); 
-} 
-if (isset($_GET["login"])&& $_GET!= null) $pdo->delete($_GET["login"]);
-if (isset($_POST["Update"])){
-    if(isset($_POST["oldlogin"])&&$_POST["oldlogin"]!=null){
-    if(isset($_POST["newName"])&& $_POST["newName"]!=null) $pdo->update("name",$_POST["newName"],$_POST["oldlogin"]);
-    if(isset($_POST["newpassword"])&& $_POST["newpassword"]!=null) $pdo->update("password",$_POST["newpassword"],$_POST["oldlogin"]);
-    if(isset($_POST["newemail"])&& $_POST["newemail"]!=null) $pdo->update("email",$_POST["newemail"],$_POST["oldlogin"]);
-    if(isset($_POST["newlogin"])&& $_POST["newlogin"]!=null) $pdo->update("login",$_POST["newlogin"],$_POST["oldlogin"]);
-    
-}
-}
-?> 
+
+while ($row = $pdostmt->fetch())
+{
+	echo $row['name']. "\n";
+}	
+echo '<div>'.$pdostmt = $pdo->Delete('user','name = "fourth"').'</div>';
+
+$pdostmt = $pdo->selectAll();
+while ($row = $pdostmt->fetch())
+{
+	echo $row['name']. "\n";
+}	
 
 
-<title>Create/update  new user</title>
-</head>
-<body>
-    <br>  Create new user <br/> 
-    <br/>
-<form name="form" method="post" action="index.php">
-    Name: <br />
-    <input name="Name" type="text" size="25" maxlength="30" value="" /> <br />
-    <br> login <br/>
-    <input name="login" type="text" size="13" maxlength="20" value="" /> <br/>
-    <br> password <br/>
-    <input name="password" type="password" size="80" maxlength="40" value=""/> <br/>
-    <br> email:<br/>
-    <input name="email" type="email" size="25" maxlength="30" value=""/>
-    <br> <br/>
-<input type="submit" name="Create" value="Create new user" />
-</form>
-    <br> Change information  <br/>
-    <br/>
-    <form name="newform" method="post" action="index.php">
-    Name: <br />
-    <input name="newName" type="text" size="25" maxlength="30" value="" /> <br />
-    <br> login <br/>
-    <input name="oldlogin" type="text" size="13" maxlength="20" value="" /> <br/>
-    <br> change login, write if you want change <br />
-    <input name="newlogin" type="text" size="13" maxlength="20" value="" /> <br/>
-    <br> password <br/>
-    <input name="newpassword" type="password" size="80" maxlength="40" value=""/> <br/>
-    <br> email:<br/>
-    <input name="newemail" type="email" size="25" maxlength="30" value=""/>
-    <br> <br/>
-<input type="submit" name="Update" value="change information " />
-</form>
-</body>
-</html>
-
+$pdo->insert('user',array('name'=>'fourth', 'password'=>"pass"));
+echo '<div>'.$pdostmt = $pdo->update('user',array('name'=>'qwerty'),'name = "fourth"').'</div>';
+$pdostmt = $pdo->selectAll();
+while ($row = $pdostmt->fetch())
+{
+	echo $row['name']. "\n";
+}	
+?>
